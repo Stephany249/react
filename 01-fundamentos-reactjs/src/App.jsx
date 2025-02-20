96125f2ccf7b25718d1dae6c19277b2d4d310fd5
Stephany <tete.1818@hotmail.com>
2025-02-20 13:50:29 -0300

    Getting started with React - Properties

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -4,9 +4,12 @@ export function App() {
   return (
     <div>
       <h1>Posts</h1>
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Step"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia nulla fuga voluptas atque quisquam accusamus facilis adipisci, quas quae ipsa. Ea vero atque iusto nostrum illo magni aliquam velit rerum!
+"
+      />
+      <Post author="Mar" content="Novo Post " />
     </div>
   );
 }
