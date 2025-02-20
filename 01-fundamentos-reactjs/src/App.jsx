82effd9b57eccc122e51bc3fa1d57b49e90234dc
Stephany <tete.1818@hotmail.com>
2025-02-20 14:18:28 -0300

    Application structure - CSS Modules

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,12 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+import "./styles.css";
 
 export function App() {
   return (
     <div>
-      <h1>Posts</h1>
+      <Header />
+
       <Post
         author="Step"
         content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia nulla fuga voluptas atque quisquam accusamus facilis adipisci, quas quae ipsa. Ea vero atque iusto nostrum illo magni aliquam velit rerum!
