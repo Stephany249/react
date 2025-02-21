1a7e80d69ae2f8f8726175b54cd2269cd1affe49
Stephany <tete.1818@hotmail.com>
2025-02-21 15:29:35 -0300

    Application structure - Component: Comment

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/stephany249.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Stephany</strong>
+              <time
+                title="21 de Fevereiro às 16:49"
+                dateTime="2025-02-21 16:49:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom Marcelo, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
