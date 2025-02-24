5e2aa9cc41f184d8671e65fca454efa1cc2b7e24
Stephany <tete.1818@hotmail.com>
2025-02-24 12:22:10 -0300

    feat: add header

--- desafio-01-todolist/src/App.tsx
+++ desafio-01-todolist/src/App.tsx
@@ -1,11 +1,14 @@
 
 import './App.css'
+import { Header } from './components/Header';
 import "./global.css";
 
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Header />
+    </div>
   )
 }
 
