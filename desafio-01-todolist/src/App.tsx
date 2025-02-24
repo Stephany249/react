40fe5eda026f4e696691c334519c299eaf132e39
Stephany <tete.1818@hotmail.com>
2025-02-24 20:26:26 -0300

    feat: add flow of adding a new item, checking/unchecking and removing

--- desafio-01-todolist/src/App.tsx
+++ desafio-01-todolist/src/App.tsx
@@ -1,28 +1,100 @@
 
+import { useState } from "react";
 import styles from "./App.module.css";
 import { Button } from "./components/Button";
 import { Header } from './components/Header';
 import { Input } from "./components/Input";
 import { Empty } from "./components/List/Empty";
 import { ListHeader } from "./components/List/ListHeader";
+import { Item } from "./components/List/Item";
 
-
+export interface Task {
+  id: number
+  text: string
+  isChecked: boolean
+}
 
 function App() {
+  const [inputValue, setInputValue] = useState('')
+  const [tasks, setTasks] = useState<Task[]>([])
+
+  function handleAddTask() {
+    if (!inputValue) {
+      return
+    }
+
+    const newTask: Task = {
+      id: new Date().getTime(),
+      text: inputValue,
+      isChecked: false,
+    }
+
+    setTasks((state) => [...state, newTask])
+    setInputValue('')
+  }
+
+  const checkedTasksCounter = tasks.reduce((prevValue, currentTask) => {
+    if (currentTask.isChecked) {
+      return prevValue + 1
+    }
+
+    return prevValue
+  }, 0)
+
+  function handleRemoveTask(id: number) {
+    const filteredTasks = tasks.filter((task) => task.id !== id)
+
+    if (!confirm('Deseja mesmo apagar essa tarefa?')) {
+      return
+    }
+
+    setTasks(filteredTasks)
+  }
+
+  function handleToggleTask({ id, value }: { id: number; value: boolean }) {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, isChecked: value }
+      }
+
+      return { ...task }
+    })
+
+    setTasks(updatedTasks)
+  }
 
   return (
     <div>
       <Header />
       <section className={styles.content}>
         <div className={styles.taskInfoContainer}>
-          <Input  />
-          <Button />
+          <Input 
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+           />
+          <Button onClick={handleAddTask} />
         </div>
 
         <div className={styles.taskListContainer}>
-          <ListHeader />
+          <ListHeader 
+             tasksCounter={tasks.length}
+             checkedTasksCounter={checkedTasksCounter}
+          />
 
-          <Empty />
+          {tasks.length === 0 ? (
+            <Empty />
+            ) : (
+            tasks.map((task) => (
+              <Item 
+                key={task.id}
+                data={task}
+                removeTask={handleRemoveTask}
+                toggleTaskStatus={handleToggleTask}
+              />
+            ))
+          )}
+
+         
         </div>
       </section>
     </div>
--- desafio-01-todolist/src/components/List/ListHeader.tsx
+++ desafio-01-todolist/src/components/List/ListHeader.tsx
@@ -1,17 +1,22 @@
 import styles from "./ListHeader.module.css";
 
+export interface ListHeaderProps {
+  tasksCounter: number
+  checkedTasksCounter: number
+}
 
-export function ListHeader() {
+
+export function ListHeader({tasksCounter, checkedTasksCounter}: ListHeaderProps) {
   return (
     <header className={styles.listHeader}>
       <aside>
         <p>Tarefas criadas</p>
-        <span>0</span>
+        <span>{tasksCounter}</span>
       </aside>
 
       <aside>
         <p>Concluídas</p>
-        <span>0</span>
+        <span> {tasksCounter === 0 ? tasksCounter: `${checkedTasksCounter} de ${tasksCounter}`}</span>
       </aside>
     </header>
   );
