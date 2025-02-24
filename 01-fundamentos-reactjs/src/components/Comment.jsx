b7c685428c82d178493b6c4dffbb8751aad8ebd5
Stephany <tete.1818@hotmail.com>
2025-02-24 10:14:40 -0300

    React engines - Liking comments

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { ThumbsUp, Trash } from "@phosphor-icons/react";
+
 import styles from "./Comment.module.css";
 import { Avatar } from "./Avatar";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -31,9 +39,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp size={20} />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
