b2123ab094e6c6b6154c6630e6610503a4334fd8
Stephany <tete.1818@hotmail.com>
2025-02-25 15:29:13 -0300

    Estrutura da aplicação - Configurando ESLint

--- 02-timer/eslint.config.js
+++ 02-timer/eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import importPlugin from 'eslint-plugin-import'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -16,6 +17,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'import': importPlugin
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -24,6 +26,24 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       '@typescript-eslint/no-empty-object-type': 'off',
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+      'sort-imports': ['error', {
+        'ignoreCase': false,
+        'ignoreDeclarationSort': true,
+        'ignoreMemberSort': false,
+        'memberSyntaxSortOrder': ['none', 'all', 'multiple', 'single'],
+        'allowSeparatedGroups': true
+      }],
+      'import/order': ['error', {
+        'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'pathGroups': [{
+          'pattern': '{react,react-dom/**,react-hooks/**,globals/**,@eslint/**,typescript-eslint/**}',
+          'group': 'external',
+          'position': 'before'
+        }],
+        'newlines-between': 'always'
+      }]
     },
   },
 )
