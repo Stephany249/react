17d4a2819340786eb1f989f455bc4592919a38b0
Stephany <tete.1818@hotmail.com>
2025-03-10 16:21:22 -0300

    Visual structure - Transaction table

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,12 +1,40 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
+
 export function Transactions() {
   return (
     <div>
       <Header />
-
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de website</td>
+              <td>
+                <PriceHighlight variant="income">R$ 17.400,00</PriceHighlight>
+              </td>
+              <td>Desenvolvimento</td>
+              <td>10/02/2021</td>
+            </tr>
+            <tr>
+              <td width="50%">Aluguel</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 1.259,00</PriceHighlight>
+              </td>
+              <td>Casa</td>
+              <td>17/02/2021</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
