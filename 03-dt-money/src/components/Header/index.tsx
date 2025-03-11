33c1c99398288696237769d0fa3e164eb280445e
Stephany <tete.1818@hotmail.com>
2025-03-11 10:58:11 -0300

    Visual structure - Creating an accessible modal

--- 03-dt-money/src/components/Header/index.tsx
+++ 03-dt-money/src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { Dialog } from 'radix-ui'
+
 import logoImg from '../../assets/logo.svg'
 
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
@@ -7,7 +9,21 @@ export function Header() {
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
