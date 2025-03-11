c5d4ce0078ea104901321fb6b4552b19dbc9eab7
Stephany <tete.1818@hotmail.com>
2025-03-11 16:37:11 -0300

    Connecting to the API - Formatting values

--- 03-dt-money/src/components/Summary/index.tsx
+++ 03-dt-money/src/components/Summary/index.tsx
@@ -6,6 +6,7 @@ import {
 import { useContext } from 'react'
 
 import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { priceFormatter } from '../../utils/formatter'
 
 import { SummaryCard, SummaryContainer } from './styles'
 
@@ -29,8 +30,6 @@ export function Summary() {
     { income: 0, outcome: 0, total: 0 },
   )
 
-  console.log(summary)
-
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -38,7 +37,7 @@ export function Summary() {
           <span>Entradas</span>
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
-        <strong>{summary.income}</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -46,7 +45,7 @@ export function Summary() {
           <span>Saídas</span>
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
-        <strong>{summary.outcome}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -54,7 +53,7 @@ export function Summary() {
           <span>Total</span>
           <CurrencyDollar size={32} color="#fff" />
         </header>
-        <strong>{summary.total}</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
