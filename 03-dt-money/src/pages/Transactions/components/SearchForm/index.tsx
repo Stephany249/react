c4260ca77a08709ead352cfbc61951f81536433b
Stephany <tete.1818@hotmail.com>
2025-03-14 10:51:19 -0300

    Performance - Applying Context Selectors

--- 03-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
+++ 03-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
@@ -2,7 +2,7 @@ import { MagnifyingGlass } from '@phosphor-icons/react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useContext } from 'react'
+import { useContextSelector } from 'use-context-selector'
 
 import { TransactionsContext } from '../../../../contexts/TransactionsContext'
 
@@ -15,7 +15,12 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-  const { fetchTransactions } = useContext(TransactionsContext)
+  const fetchTransactions = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.fetchTransactions
+    },
+  )
 
   const {
     register,
