3fa46f7d4bb3f332917aa4a80d457c21d40629e0
Stephany <tete.1818@hotmail.com>
2025-03-19 12:12:02 -0300

    Project Setup - Creating React App with Vite

--- 04-pizzashop-web/eslint.config.js
+++ 04-pizzashop-web/eslint.config.js
@@ -3,6 +3,8 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import importPlugin from 'eslint-plugin-import'
+import prettier from 'eslint-plugin-prettier'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -16,6 +18,8 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'import': importPlugin,
+      'prettier': prettier
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +27,28 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+      'sort-imports': ['error', {
+        'ignoreCase': false,
+        'ignoreDeclarationSort': true,
+        'ignoreMemberSort': false,
+        'memberSyntaxSortOrder': ['none', 'all', 'multiple', 'single'],
+        'allowSeparatedGroups': true
+      }],
+      'import/order': ['error', {
+        'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'pathGroups': [{
+          'pattern': '{react,react-dom/**,react-hooks/**,globals/**,@eslint/**,typescript-eslint/**}',
+          'group': 'external',
+          'position': 'before'
+        }],
+        'newlines-between': 'always'
+      }],
+      'max-len': ['error', { 'code': 80 }],
+      'prettier/prettier': ['error', { 'singleQuote': true, 'semi': false, 'printWidth': 80 }]
     },
   },
 )
