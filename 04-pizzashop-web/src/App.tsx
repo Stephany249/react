d23e3db6e4637e46954ae749746c6c4e856bd28d
Stephany <tete.1818@hotmail.com>
2025-03-20 14:32:14 -0300

    Pages and Components (UI) - ThemeToggle and Account Menu

--- 04-pizzashop-web/src/App.tsx
+++ 04-pizzashop-web/src/App.tsx
@@ -5,13 +5,16 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'sonner'
 
 import { router } from './routes'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Toaster richColors />
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizza.shop:theme" defaultTheme="dark">
+        <Toaster richColors />
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
