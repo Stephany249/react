416523752417173afc62a041fa416f9699dabc35
Stephany <tete.1818@hotmail.com>
2025-03-20 10:49:46 -0300

    Pages and components (UI) - Page: Restaurant registration

--- 04-pizzashop-web/src/routes.tsx
+++ 04-pizzashop-web/src/routes.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/signIn'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
+import { SignUp } from './pages/auth/signup'
 
 export const router = createBrowserRouter([
   {
@@ -17,12 +18,16 @@ export const router = createBrowserRouter([
     ],
   },
   {
-    path: '/sign-in',
+    path: '/',
     element: <AuthLayout />,
     children: [
       {
         path: '/sign-in',
         element: <SignIn />,
+      },
+      {
+        path: '/sign-up',
+        element: <SignUp />,
       },
     ],
   },
