5e731a40cd3bef41718d2c7ee2b29d0660bf1ca6
Stephany <tete.1818@hotmail.com>
2025-03-20 15:13:35 -0300

    Pages and components (UI) - Page: Order listing

--- 04-pizzashop-web/src/routes.tsx
+++ 04-pizzashop-web/src/routes.tsx
@@ -5,6 +5,7 @@ import { SignIn } from './pages/auth/signIn'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { SignUp } from './pages/auth/signup'
+import { Orders } from './pages/app/orders/orders'
 
 export const router = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <Dashboard />,
+      },
+      {
+        path: '/orders',
+        element: <Orders />,
       },
     ],
   },
