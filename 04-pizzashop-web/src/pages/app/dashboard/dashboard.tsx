6eefd159010f3feb103099f0bd0c5f0a95826bb2
Stephany <tete.1818@hotmail.com>
2025-03-21 16:35:00 -0300

    Pages and Components (UI) - Popular Products Chart

--- 04-pizzashop-web/src/pages/app/dashboard/dashboard.tsx
+++ 04-pizzashop-web/src/pages/app/dashboard/dashboard.tsx
@@ -5,6 +5,7 @@ import { MonthOrdersAmountCard } from './monthOrdersAmountCard'
 import { DayOrdersAmountCard } from './dayOrdersAmountCard'
 import { MonthCanceledOrdersAmountCard } from './monthCanceledOrdersAmountCard'
 import { RevenueChart } from './revenueChart'
+import { PopularProductsChart } from './popularProductsChart'
 
 export function Dashboard() {
   return (
@@ -21,6 +22,7 @@ export function Dashboard() {
 
         <div className="grid grid-cols-9 gap-4">
           <RevenueChart />
+          <PopularProductsChart />
         </div>
       </div>
     </>
