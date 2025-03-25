4df5131333aa1cab72506560229f349fe3872702
Stephany <tete.1818@hotmail.com>
2025-03-25 15:29:41 -0300

    Connection to API - Revenue in the period

--- 04-pizzashop-web/src/pages/app/dashboard/revenueChart.tsx
+++ 04-pizzashop-web/src/pages/app/dashboard/revenueChart.tsx
@@ -7,6 +7,10 @@ import {
   YAxis,
 } from 'recharts'
 import colors from 'tailwindcss/colors'
+import { useQuery } from '@tanstack/react-query'
+import { useMemo, useState } from 'react'
+import { subDays } from 'date-fns'
+import { DateRange } from 'react-day-picker'
 
 import {
   Card,
@@ -15,18 +19,34 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
-
-const data = [
-  { date: '10/03', revenue: 1200 },
-  { date: '11/03', revenue: 800 },
-  { date: '12/03', revenue: 900 },
-  { date: '13/03', revenue: 400 },
-  { date: '14/03', revenue: 2300 },
-  { date: '15/03', revenue: 800 },
-  { date: '16/03', revenue: 640 },
-]
+import { getDailyRevenueInPeriod } from '@/api/getDailyRevenueInPeriod'
+import { Label } from '@/components/ui/label'
+import { DatePickerWithRange } from '@/components/ui/date-range-picker'
 
 export function RevenueChart() {
+  const [dateRange, setDateRange] = useState<DateRange | undefined>({
+    from: subDays(new Date(), 7),
+    to: new Date(),
+  })
+
+  const { data: dailyRevenueInPeriod } = useQuery({
+    queryKey: ['metrics', 'dailyRevenueInPeriod', dateRange],
+    queryFn: () =>
+      getDailyRevenueInPeriod({
+        from: dateRange?.from,
+        to: dateRange?.to,
+      }),
+  })
+
+  const chartData = useMemo(() => {
+    return dailyRevenueInPeriod?.map((chartItem) => {
+      return {
+        date: chartItem.date,
+        receipt: chartItem.receipt / 100,
+      }
+    })
+  }, [dailyRevenueInPeriod])
+
   return (
     <Card className="col-span-6">
       <CardHeader className="flex-col items-center justify-between pb-8">
@@ -36,34 +56,38 @@ export function RevenueChart() {
           </CardTitle>
           <CardDescription>Receita diária no período </CardDescription>
         </div>
+        <div className="flex items-center gap-3">
+          <Label>Período</Label>
+          <DatePickerWithRange date={dateRange} onDateChange={setDateRange} />
+        </div>
       </CardHeader>
       <CardContent>
-        <ResponsiveContainer width="100%" height={240}>
-          <LineChart data={data} style={{ fontSize: 12 }}>
-            <XAxis dataKey="date" axisLine={false} tickLine={false} dy={16} />
-            <YAxis
-              stroke="#888"
-              axisLine={false}
-              tickLine={false}
-              width={80}
-              tickFormatter={(value: number) =>
-                value.toLocaleString('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL',
-                })
-              }
-            />
-
-            <CartesianGrid vertical={false} className="stroke-muted" />
-
-            <Line
-              stroke={colors.rose[500]}
-              type="linear"
-              strokeWidth={2}
-              dataKey="revenue"
-            />
-          </LineChart>
-        </ResponsiveContainer>
+        {chartData && (
+          <ResponsiveContainer width="100%" height={240}>
+            <LineChart data={chartData} style={{ fontSize: 12 }}>
+              <XAxis dataKey="date" axisLine={false} tickLine={false} dy={16} />
+              <YAxis
+                stroke="#888"
+                axisLine={false}
+                tickLine={false}
+                width={80}
+                tickFormatter={(value: number) =>
+                  value.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  })
+                }
+              />
+              <CartesianGrid vertical={false} className="stroke-muted" />
+              <Line
+                stroke={colors.violet[500]}
+                type="linear"
+                strokeWidth={2}
+                dataKey="receipt"
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        )}
       </CardContent>
     </Card>
   )
