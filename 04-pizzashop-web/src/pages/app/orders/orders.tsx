389adb8ad7d47420b8f44552720ae1960a0f63d2
Stephany <tete.1818@hotmail.com>
2025-03-25 12:05:46 -0300

    Connection to API - Order pagination

--- 04-pizzashop-web/src/pages/app/orders/orders.tsx
+++ 04-pizzashop-web/src/pages/app/orders/orders.tsx
@@ -1,5 +1,7 @@
 import { Helmet } from 'react-helmet-async'
 import { useQuery } from '@tanstack/react-query'
+import { useSearchParams } from 'react-router-dom'
+import { z } from 'zod'
 
 import { OrderTableRow } from './orderTableRow'
 import { OrderTableFilters } from './orderTableFilters'
@@ -15,10 +17,26 @@ import { Pagination } from '@/components/pagination'
 import { getOrders } from '@/api/getOrders'
 
 export function Orders() {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const pageIndex = z.coerce
+    .number()
+    .transform((page) => page - 1)
+    .parse(searchParams.get('page') ?? '1')
+
   const { data: result } = useQuery({
-    queryKey: ['orders'],
-    queryFn: getOrders,
+    queryKey: ['orders', pageIndex],
+    queryFn: () => getOrders({ pageIndex }),
   })
+
+  function handlePaginate(pageIndex: number) {
+    setSearchParams((state) => {
+      state.set('page', (pageIndex + 1).toString())
+
+      return state
+    })
+  }
+
   return (
     <>
       <Helmet title="Pedidos" />
@@ -49,7 +67,14 @@ export function Orders() {
               </TableBody>
             </Table>
           </div>
-          <Pagination pageIndex={0} totalCount={105} perPage={10} />
+          {result && (
+            <Pagination
+              onPageChange={handlePaginate}
+              pageIndex={result.meta.pageIndex}
+              totalCount={result.meta.totalCount}
+              perPage={result.meta.perPage}
+            />
+          )}
         </div>
       </div>
     </>
