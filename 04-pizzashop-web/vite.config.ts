3463bf340faabd348cd30d26b6ef6ee4c661e0d3
Stephany <tete.1818@hotmail.com>
2025-03-26 17:07:09 -0300

    Tests and mocks - Unit testing: Order Status

--- 04-pizzashop-web/vite.config.ts
+++ 04-pizzashop-web/vite.config.ts
@@ -1,9 +1,8 @@
 import path from 'node:path'
 
 import tailwindcss from '@tailwindcss/vite'
-import { UserConfig, defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import type { InlineConfig } from 'vitest/node'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,11 +12,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  test: {
-    globals: true,
-    setupFiles: ['./test/setupTests.ts'],
-    environment: 'happy-dom',
-  },
-} as UserConfig & {
-  test: InlineConfig
 })
