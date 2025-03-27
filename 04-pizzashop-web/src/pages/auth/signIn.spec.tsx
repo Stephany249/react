869ee280aea78041546165c01148cdf4906d01f1
Stephany <tete.1818@hotmail.com>
2025-03-27 14:42:19 -0300

    Tests and mocks - Unit testing: Sign In

---
+++ 04-pizzashop-web/src/pages/auth/signIn.spec.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable max-len */
+import { QueryClientProvider } from '@tanstack/react-query'
+import { render } from '@testing-library/react'
+import { HelmetProvider } from 'react-helmet-async'
+import { MemoryRouter } from 'react-router-dom'
+
+import { SignIn } from './signIn'
+
+import { queryClient } from '@/lib/reactQuery'
+
+describe('SignIn', () => {
+  it('should set default email input value if email is present on search params', () => {
+    const wrapper = render(<SignIn />, {
+      wrapper: ({ children }) => {
+        return (
+          <HelmetProvider>
+            <MemoryRouter
+              initialEntries={['/sign-in?email=janedoe@example.com']}
+            >
+              <QueryClientProvider client={queryClient}>
+                {children}
+              </QueryClientProvider>
+            </MemoryRouter>
+          </HelmetProvider>
+        )
+      },
+    })
+
+    const emailInput = wrapper.getByLabelText('Seu e-mail') as HTMLInputElement
+
+    expect(emailInput.value).toEqual('janedoe@example.com')
+  })
+})
