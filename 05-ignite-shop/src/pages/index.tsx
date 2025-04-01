8730a2421b22a4770bdd20031e0a1f24a604b80f
Stephany <tete.1818@hotmail.com>
2025-04-01 16:41:39 -0300

    Integration with Stripe - Data Fetching in Next.js

--- 05-ignite-shop/src/pages/index.tsx
+++ 05-ignite-shop/src/pages/index.tsx
@@ -8,7 +8,7 @@ import camiseta3 from '../assets/camisetas/3.png'
 
 import { HomeContainer, Product } from '@/styles/pages/home'
 
-export default function Home() {
+export default function Home(props) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -18,6 +18,7 @@ export default function Home() {
 
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
+      <pre>{JSON.stringify(props.list)}</pre>
       <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt={''} />
         <footer>
@@ -51,4 +52,16 @@ export default function Home() {
       </Product>
     </HomeContainer>
   )
+}
+
+export const getServerSideProps = async () => {
+  await new Promise(resolve => setTimeout(resolve, 2000))
+
+  console.log('rodou')
+
+  return {
+    props: {
+      list: [1,2,3]
+    }
+  }
 }
