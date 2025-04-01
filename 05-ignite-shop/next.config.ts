973390dde2fa8a1cc7f6012a967e2c6c0e5d8791
Stephany <tete.1818@hotmail.com>
2025-04-01 16:59:34 -0300

    Stripe Integration - Searching for Stripe products

--- 05-ignite-shop/next.config.ts
+++ 05-ignite-shop/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
+    domains: ['files.stripe.com'],
     localPatterns: [
       {
         pathname: '/assets/**',
