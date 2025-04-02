426f8fc05bf365d268bd5d5cbf977a628b90d145
Stephany <tete.1818@hotmail.com>
2025-04-02 15:14:54 -0300

    Product & Checkout - Checkout on Stripe

--- 05-ignite-shop/src/pages/product/[id].tsx
+++ 05-ignite-shop/src/pages/product/[id].tsx
@@ -17,10 +17,15 @@ interface ProductProps {
     imageUrl: string
     price: string
     description: string
+    defaultPriceId: string
   }
 }
 
 export default function Product({ product }: ProductProps) {
+  function handleBuyProduct() {
+    console.log(product.defaultPriceId)
+  }
+
   return (
     <ProductContainer>
       <ImageContainer>
@@ -32,7 +37,7 @@ export default function Product({ product }: ProductProps) {
         <span>{product.price}</span>
         <p>{product.description}</p>
 
-        <button type="button">Comprar agora </button>
+        <button onClick={handleBuyProduct}>Comprar agora </button>
       </ProductDetails>
     </ProductContainer>
   )
@@ -71,6 +76,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
           currency: 'BRL',
         }).format(price.unit_amount / 100),
         description: product.description,
+        defaultPriceId: price.id,
       },
     },
     revalidate: 60 * 60 * 1, // 1 hours
