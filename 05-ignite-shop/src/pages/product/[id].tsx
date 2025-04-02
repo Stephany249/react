69cd5e8f9f612a43e8af29dde433d0b483390373
Stephany <tete.1818@hotmail.com>
2025-04-02 14:17:39 -0300

    Product & Checkout - Uploading product data

--- 05-ignite-shop/src/pages/product/[id].tsx
+++ 05-ignite-shop/src/pages/product/[id].tsx
@@ -1,31 +1,66 @@
-import { useRouter } from 'next/router'
+import Image from 'next/image'
+import { GetStaticProps } from 'next'
+import Stripe from 'stripe'
 
 import {
   ImageContainer,
   ProductContainer,
   ProductDetails,
 } from '@/styles/pages/product'
+import { stripe } from '@/lib/stripe'
 
-export default function Product() {
-  const { query } = useRouter()
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+  }
+}
 
+export default function Product({ product }: ProductProps) {
   return (
     <ProductContainer>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt={''} />
+      </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79.90</span>
-        <p>
-          {' '}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-          consequatur, deleniti voluptatum voluptatem unde corrupti, distinctio
-          aliquam impedit laudantium eos incidunt ad vero quam, veniam officiis
-          dicta eveniet doloribus aliquid.
-        </p>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
 
         <button type="button">Comprar agora </button>
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params.id
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount / 100),
+        description: product.description,
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1 hours
+  }
 }
