e5f46c9db7f5394860b217cf007c913e2a80e607
Stephany <tete.1818@hotmail.com>
2025-04-02 14:02:09 -0300

    Product & Checkout - Product page structure

--- 05-ignite-shop/src/pages/product/[id].tsx
+++ 05-ignite-shop/src/pages/product/[id].tsx
@@ -1,7 +1,31 @@
 import { useRouter } from 'next/router'
 
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/styles/pages/product'
+
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79.90</span>
+        <p>
+          {' '}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+          consequatur, deleniti voluptatum voluptatem unde corrupti, distinctio
+          aliquam impedit laudantium eos incidunt ad vero quam, veniam officiis
+          dicta eveniet doloribus aliquid.
+        </p>
+
+        <button type="button">Comprar agora </button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
