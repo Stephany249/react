1fd64e84b376b550777a98079fc7b16ad69dd296
Stephany <tete.1818@hotmail.com>
2025-04-04 11:05:21 -0300

    Implementing a shopping cart in the application

--- 05-ignite-shop/src/pages/_app.tsx
+++ 05-ignite-shop/src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/app'
 import Image from 'next/image'
+import { Handbag } from 'phosphor-react'
+import { useState } from 'react'
 
 import logoImg from '../assets/logo.svg'
 
@@ -9,10 +11,17 @@ import { Container, Header } from '@/styles/pages/app'
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [test, setTest] = useState(true)
+
   return (
     <Container>
       <Header>
         <Image src={logoImg.src} alt="" width={130} height={52} />
+
+        <button>
+          <Handbag size={24} weight="bold" />
+          {test && <span>1</span>}
+        </button>
       </Header>
       <Component {...pageProps} />
     </Container>
--- 05-ignite-shop/src/styles/pages/app.ts
+++ 05-ignite-shop/src/styles/pages/app.ts
@@ -13,4 +13,41 @@ export const Header = styled('header', {
   width: '100%',
   maxWidth: 1180,
   margin: '0 auto',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+
+  button: {
+    background: '$gray800',
+    border: 0,
+    borderRadius: 6,
+    padding: '0.75rem',
+    cursor: 'pointer',
+    position: 'relative',
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    svg: {
+      color: '$gray700',
+    },
+
+    span: {
+      position: 'absolute',
+      right: '-10px',
+      top: '-10px',
+      backgroundColor: '$green500',
+      color: '$white',
+      width: '1.25rem',
+      height: '1.25rem',
+      borderRadius: '50%',
+      fontSize: '$sm',
+      fontWeight: 'bold',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  },
 })
